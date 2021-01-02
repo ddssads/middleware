e3c29e7b7f450f8c41ff9a9d4b445e5a22a9e8b7
ddssads <tw0937608360@gmail.com>
2021-01-02 11:16:01 +0800

    add middleware

---
+++ app.js
@@ -0,0 +1,34 @@
+// app.js
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(function (req, res, next) {
+  const reqTime = new Date()
+  res.on('finish', () => {
+    const resTime = new Date()
+    const totalTime = resTime - reqTime
+    console.log(`reqtime: ${reqTime.toLocaleString()} || restime: ${resTime.toLocaleString()} ${req.method} from ${req.url} total time:${totalTime}ms`)
+  })
+  return next()
+})
+
+app.get('/', (req, res) => {
+  res.send('列出全部 Todo')
+})
+
+app.get('/new', (req, res) => {
+  res.send('新增 Todo 頁面')
+})
+
+app.get('/:id', (req, res) => {
+  res.send('顯示一筆 Todo')
+})
+
+app.post('/', (req, res) => {
+  res.send('新增一筆  Todo')
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`)
+})
